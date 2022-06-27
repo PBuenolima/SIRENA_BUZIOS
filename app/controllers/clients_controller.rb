5c85be5b7bd5324d19202ec97a1e2577d96a08f8
Pedro Bueno <pbuenolima@gmail.com>
2022-06-27 12:59:37 -0300

    Navbar

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,14 @@
 class ClientsController < ApplicationController
-
   def create
+    @client = Client.new(client_params)
+    @client.save
 
+    redirect_to root_path
+  end
+
+  private
+
+  def client_params
+    params.require(:client).permit(:name, :email, :phone_number)
   end
 end
